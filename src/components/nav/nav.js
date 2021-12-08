00522d7409b16ec44301f3e08e4c6b3b7aa1c1b0
MarcoTorr <marco.torres@aiesec.net>
2021-12-08 13:27:01 -0600

    Routing update

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -7,12 +7,12 @@ function Navigationbar() {
   return (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
     <Container>
-    <Navbar.Brand href="/">MARCO TORRES</Navbar.Brand>
+    <Navbar.Brand href="/Portfolio_React">MARCO TORRES</Navbar.Brand>
     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
     <Navbar.Collapse id="responsive-navbar-nav">
       <Nav className="me-auto">
         <Nav.Link href="/projects">Projects</Nav.Link>
-        <Nav.Link href="/">About me</Nav.Link>
+        <Nav.Link href="/Portfolio_React">About me</Nav.Link>
         <Nav.Link href="/resume">Resume</Nav.Link>
       </Nav>
       <Nav>
